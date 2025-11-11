5fd46d80e2b19dfc7c2458be399011a89ce5a6b8
adi <90326965+adithayyil@users.noreply.github.com>
2025-11-11 09:51:36 -0500

    feat: JobStoreConnector abstraction

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,3 +1,4 @@
 from .pinecone_connector import PineconeConnector
+from .job_store_connector import JobStoreConnector
 
-__all__ = ['PineconeConnector']
+__all__ = ['PineconeConnector', 'JobStoreConnector']
