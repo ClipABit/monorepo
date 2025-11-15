a13558391cc3963381cddd1ede8f1beefd7a6693
eshaan-mehta <eshaanm.mehta@gmail.com>
2025-11-15 17:20:58 -0500

    began first iteration of connector

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,4 +1,6 @@
 from .pinecone_connector import PineconeConnector
 from .job_store_connector import JobStoreConnector
+from .r2_connector import R2Connector
 
-__all__ = ['PineconeConnector', 'JobStoreConnector']
+
+__all__ = ['PineconeConnector', 'JobStoreConnector', 'R2Connector']
