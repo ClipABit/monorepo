3116dd831b98a80dce04b129bafb80f9a7b9ef57
Yifan Zhang <yifanzhang0905@gmail.com>
2025-11-23 23:12:01 -0500

    fix style

--- backend/face_recognition/face.py
+++ backend/face_recognition/face.py
@@ -8,10 +8,10 @@ class Face:
 
     @classmethod
     def from_original_image(cls, embedding: np.ndarray, orig_image: np.ndarray | str, bbox: tuple[int, int, int, int]):
-        if (type(orig_image) == str):
+        if (isinstance(orig_image, str)):
             image_np = cv2.imread(orig_image)
             image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-        elif (type(orig_image) == np.ndarray):
+        elif (isinstance(orig_image, np.ndarray)):
             image_np = orig_image
         else:
             raise ValueError("orig_image must be either a file path (str) or a numpy ndarray.")
