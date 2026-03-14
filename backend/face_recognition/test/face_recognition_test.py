e6cee9c8dce1bad5e20715132680e73328d0209b
Yifan Zhang <yifanzhang0905@gmail.com>
2026-03-14 11:56:18 -0400

    update test

--- backend/face_recognition/test/face_recognition_test.py
+++ backend/face_recognition/test/face_recognition_test.py
@@ -1,17 +1,32 @@
 import os
 import sys
+from PIL import Image
+import numpy as np
+import cv2
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-from face_recognition import FaceRepository
+from face_recognition import *
 
 
 def main():
-	fr = FaceRepository(
-		pinecone_api_key=os.getenv("PINECONE_API_KEY"),
-		index_name="face-index"
-    )
-	faces = fr.add_images(namespace="test", chunk_id="chunk_0", img_lst=["photo1.jpg"])
+	face_detector = FaceDetector(detector_backend="retinaface")
+	# img_path = "test_img_2faces.png"
+	img_path = "/Users/yifanzhang/workspace/ClipABit/monorepo/backend/face_recognition/test/frame_images/saved_image_19.png"
+	# img = Image.open(img_path)
+	# arr = np.array(img)
+	# # arr = cv2.cvtColor(arr, cv2.COLOR_RGB2BGR)
+	# faces = face_detector.detect_and_embed(img=arr)
+	# faces = face_detector.detect_and_embed(img_path)
+	faces = face_detector.detect_and_embed(np.load("/Users/yifanzhang/workspace/ClipABit/monorepo/backend/face_recognition/test/frame_images/img_array19.npy"))
+	
+	arr = np.load("/Users/yifanzhang/workspace/ClipABit/monorepo/backend/face_recognition/test/frame_images/img_array19.npy")
+	# print("Loaded info:", type(arr), arr.shape, arr.dtype, np.min(arr), np.max(arr))
+	print("Loaded top-left pixel:", arr[0,0])
+
+	# print(arr)
+
+
 	print(faces)
 
 if __name__ == "__main__":
